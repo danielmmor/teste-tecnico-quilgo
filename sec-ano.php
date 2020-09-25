74e0b680d516c5817bf7d4d60f03bc7a43e6f178
Dan Moreira <dm-moreira@hotmail.com>
2020-09-25 11:57:34 -0300

    refactor sec-ano

--- sec-ano.php
+++ sec-ano.php
@@ -1,13 +1,12 @@
 <?php
-$ano = readline('Insira o ano: ');
-$ano_n = (int)$ano;
-if ($ano_n <= 100) {
-    $sec = 1;
-} elseif ($ano_n % 100 == 0) {
-    $sec = $ano_n/100;
-} else {
-    $sec = $ano_n/100 + 1;
+function SeculoAno($ano) { 
+    $ano_n = (int)$ano;
+    if ($ano_n % 100 == 0) {
+        $sec = $ano_n/100;
+    } else {
+        $sec = $ano_n/100 + 1;
+    }
+    $text = 'Ano '.$ano.' = século '.strval((int)$sec);
+    echo $text;
 }
-$text = 'Ano '.$ano.' = século '.strval((int)$sec);
-echo $text;
 ?>
